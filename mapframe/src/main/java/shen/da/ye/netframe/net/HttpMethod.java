47a021c5ae63f6f43a441608a22b933bd807d061
yedashen <122649796@qq.com>
2017-11-08 08:40:49 +0800

    用Enum定义一些基本的请求方式

---
+++ mapframe/src/main/java/shen/da/ye/netframe/net/HttpMethod.java
@@ -0,0 +1,16 @@
+package shen.da.ye.netframe.net;
+
+/**
+ * @author ChenYe
+ *         created by on 2017/11/8 0008. 08:39
+ **/
+
+public enum HttpMethod {
+    GET,
+    POST,
+    POST_RAW,
+    PUT,
+    PUT_RAW,
+    DELETE,
+    UPLOAD
+}
